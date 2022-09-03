b549f595894eb5e025800a4a8fd129029f7ee9ce
DenSSS <dimon.yablonovskiyy@mail.ru>
2022-09-03 15:58:47 +0300

    first pull

---
+++ pj/main_page/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, redirect
+from django.http import HttpRequest, HttpResponse, JsonResponse, HttpResponseRedirect
+from django.contrib.auth import logout
+import json
+import requests
+from django.contrib.auth.models import User, Group
+# from .models import BuyStatus, PrimeStatus
+# from django import forms
+# from .forms import *
+
+
+def main_home(request):
+    return render(request, 'main_page/main_page.html')

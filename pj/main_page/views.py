04da7b11c072d3a33e7201496bb61fd28979240d
linum1337 <lebishova.elena@mail.ru>
2022-09-05 10:36:36 +0300

    сосать блять

--- pj/main_page/views.py
+++ pj/main_page/views.py
@@ -5,6 +5,7 @@ import json
 import requests
 from django.contrib.auth.models import User, Group
 from .models import Disciplines
+from .models import DisciplinesActual
 # from django import forms
 # from .forms import *
 
@@ -16,4 +17,5 @@ def disciplines(request):
 
 def discipline_page(request, discipline):
     discipline_bd = Disciplines.objects.get(eng_discipline=discipline)
-    return render(request, 'main_page/discipline_page.html', {"discipline":discipline_bd})
+    date_db = DisciplinesActual.objects.get(eng_discipline=discipline)
+    return render(request, 'main_page/discipline_page.html', {"discipline":discipline_bd, "date":date_db})

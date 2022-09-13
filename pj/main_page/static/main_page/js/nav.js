8e2a4f9ddcd800fd760ad2431611a3c4c64b8c8c
DenSSS <dimon.yablonovskiyy@mail.ru>
2022-09-13 14:37:46 +0300

    Обновил мобильный дизайн

--- pj/main_page/static/main_page/js/nav.js
+++ pj/main_page/static/main_page/js/nav.js
@@ -2,13 +2,12 @@ function open_burger(x){
     if (x=='burger'){
         if (document.getElementById(x).classList.contains('burger_hidden')){
             document.getElementById(x).classList.remove('burger_hidden');
+            // document.getElementById(x).classList.remove('burger_hidden');
             document.getElementById('open1').classList.add('open-svg');
-            // document.getElementById('open1').src="/static/main_page/img/open_after.png";
         }
         else{
             document.getElementById(x).classList.add('burger_hidden');
             document.getElementById('open1').classList.remove('open-svg');
-            // document.getElementById('open1').src="/static/main_page/img/open.png";
         }
     }
 };
